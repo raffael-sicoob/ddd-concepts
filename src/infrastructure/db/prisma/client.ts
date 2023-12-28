5060554ec0049e8e659d2ba3d3c09ef30af781d0
darley-raffael <darley.farias@segurosicoob.com.br>
2023-12-28 09:32:56 -0300

    refactor: replace command bunx to npx

--- src/infrastructure/db/prisma/client.ts
+++ src/infrastructure/db/prisma/client.ts
@@ -1,9 +1,11 @@
-import { spawnSync } from "child_process";
 import { PrismaClient } from "@prisma/client";
+import { spawnSync } from "child_process";
 
 export const prisma = new PrismaClient();
 
 if (Bun.env.NODE_ENV === "test") {
 	// executar o comando de criação de tabela
-	spawnSync("bunx", ["prisma", "migrate", "deploy"], { stdio: "inherit" });
+	spawnSync("npx", ["prisma", "migrate", "deploy"], { stdio: "inherit" });
+
+	console.log("Tabelas criadas");
 }
